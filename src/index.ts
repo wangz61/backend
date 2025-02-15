9fbef7bfac2eae0cf4d982db9e9d62777300c76c
wangz61 <18582378131@163.com>
2025-02-14 21:27:39 -0500

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
